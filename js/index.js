2c187416207fb2289c4ddb2ac95ce015d039bc4d
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2023-11-16 18:07:04 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
-// EXPANDINDO AS DIV's 
-function toggleExpansion() {
-  var content = document.querySelector('.expansion-content');
-  content.style.display = (content.style.display === '' || content.style.display === 'none') ? 'block' : 'none';
+// CONTROLANDO A VISIBILIDADE COM CLASSES
+function toggleExpansion(className) {
+  var content = document.querySelector('.' + className);
+  content.classList.toggle('visible');
 }
 
-function toggleExpansionSobre() {
-  var content = document.querySelector('.expansion-contentSobre');
-  content.style.display = (content.style.display === '' || content.style.display === 'none') ? 'block' : 'none';
-}
+// VALIDÇÃO DE FORMULARIO
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  var nameInput = document.getElementById('name');
+  var emailInput = document.getElementById('email');
+  var messageInput = document.getElementById('message');
+
+  // LOGICA DE VALIDAÇAO
+  if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || messageInput.value.trim() === ''){
+    alert('Por favor, preencha todos os campos.');
+    event.preventDefault();
+  };
+});
