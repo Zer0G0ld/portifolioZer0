939418e34968c6a3c438ff3ae8a68099be514f01
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2023-11-16 12:15:33 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ function toggleExpansion() {
   var content = document.querySelector('.expansion-content');
   content.style.display = (content.style.display === '' || content.style.display === 'none') ? 'block' : 'none';
 }
+
+function toggleExpansionSobre() {
+  var content = document.querySelector('.expansion-contentSobre');
+  content.style.display = (content.style.display === '' || content.style.display === 'none') ? 'block' : 'none';
+}
