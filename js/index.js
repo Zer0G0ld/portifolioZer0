2a75efb835c36939aca38fb00d59c5cc4073d376
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2023-11-16 10:05:29 -0300

    Create index.js
    criei uma pasta para o javascript para ficar mais legal

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// EXPANDINDO AS DIV's 
+function toggleExpansion() {
+  var content = document.querySelector('.expansion-content');
+  content.style.display = (content.style.display === '' || content.style.display === 'none') ? 'block' : 'none';
+}
