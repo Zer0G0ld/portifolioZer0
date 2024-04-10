0458e7dc7029b2a7329146b0aa52a17656132724
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2024-04-10 21:17:09 +0000

    update

--- js/index.js
+++ js/index.js
@@ -4,15 +4,16 @@ function toggleExpansion(className) {
   content.classList.toggle('visible');
 }
 
-// VALIDÇÃO DE FORMULARIO
+
+// VALIDAÇÃO DE FORMULÁRIO
 document.getElementById('contact-form').addEventListener('submit', function (event) {
   var nameInput = document.getElementById('name');
   var emailInput = document.getElementById('email');
   var messageInput = document.getElementById('message');
 
-  // LOGICA DE VALIDAÇAO
-  if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || messageInput.value.trim() === ''){
-    alert('Por favor, preencha todos os campos.');
-    event.preventDefault();
-  };
+  // LÓGICA DE VALIDAÇÃO
+  if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || messageInput.value.trim() === '') {
+      alert('Por favor, preencha todos os campos.');
+      event.preventDefault();
+  }
 });
